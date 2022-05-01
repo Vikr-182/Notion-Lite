1106b452204181ed2a44c96e15117f04181c8b53
pranav-kirsur <pranav.kirsur@gmail.com>
2022-05-01 16:37:06 +0530

    Name consistency

--- src/newsession.js
+++ src/newsession.js
@@ -29,7 +29,7 @@ function App(id) {
 		<div id="landing-screen" className="App">
 		<Toggle theme={theme} toggleTheme={themeToggler} />
 		<div>
-		<h1>Notion</h1>
+		<h1>Notion-Lite</h1>
 		<h4>Join Session</h4>
 		</div>
 
